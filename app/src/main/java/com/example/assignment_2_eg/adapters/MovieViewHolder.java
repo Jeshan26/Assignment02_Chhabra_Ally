e5088cd66b5519dfcc66826f9da213ce4951aa5f
Aaliyah Ally <79179237+aaliyah033@users.noreply.github.com>
2025-03-28 00:05:08 -0400

    Update MovieViewHolder.java

--- app/src/main/java/com/example/assignment_2_eg/adapters/MovieViewHolder.java
+++ app/src/main/java/com/example/assignment_2_eg/adapters/MovieViewHolder.java
@@ -16,26 +16,30 @@ import java.util.ArrayList;
 
 public class MovieViewHolder extends RecyclerView.ViewHolder {
 
-
+// Declared views to hold movie data
     ImageView imageView;
     TextView title;
     TextView year;
 
-    MovieClickListener movieClickListener;
-
+    MovieClickListener movieClickListener; //to handle click events
+    
+    // Constructor to initialize the views and set the click listener
     public MovieViewHolder(@NonNull View itemView, MovieClickListener movieClickListener) {
         super(itemView);
+        
+        // Initialize the views by finding them from the item layout
         imageView = itemView.findViewById(R.id.imagePosterView);
         title = itemView.findViewById(R.id.movie_title);
         year = itemView.findViewById(R.id.movie_year);
 
-        this.movieClickListener = movieClickListener;
+        this.movieClickListener = movieClickListener; // Store the click listener to be used
 
+         // onClickListener for the entire item view
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("tag", "onViewHolder Click");
-                movieClickListener.onClick(v,getAdapterPosition());
+                movieClickListener.onClick(v,getAdapterPosition());  // Call the onClick method of the listener, passing the clicked view and its position
             }
         });
     }

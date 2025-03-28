5e17a6f9f42b08107b29f6f17fc6d1a67d343419
Aaliyah Ally <79179237+aaliyah033@users.noreply.github.com>
2025-03-28 00:08:40 -0400

    Update ApiClient.java

--- app/src/main/java/com/example/assignment_2_eg/utils/ApiClient.java
+++ app/src/main/java/com/example/assignment_2_eg/utils/ApiClient.java
@@ -7,14 +7,15 @@ import okhttp3.Callback;
 
 public class ApiClient {
 
-    private static final OkHttpClient client = new OkHttpClient();
-    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private static final OkHttpClient client = new OkHttpClient(); //OkHttpClient instance for network requests
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8"); //JSON request
 
+    //get movie data from a given URL
     public static void getMovies(String url , Callback callback){
-        Request request =  new Request.Builder()
-                .url(url)
-                .build();
+        Request request =  new Request.Builder() // Build the request with the provided URL
+                .url(url) // Set the request URL
+                .build(); // Build the request object
 
-        client.newCall(request).enqueue(callback);
+        client.newCall(request).enqueue(callback); // this Makes the request and handle the response in the callback
     }
 }

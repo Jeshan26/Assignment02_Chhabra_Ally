a6d771da93f2bed607d7a46997d6224148b2bb4f
Aaliyah Ally <79179237+aaliyah033@users.noreply.github.com>
2025-03-28 00:13:42 -0400

    Update MovieDetails.java

--- app/src/main/java/com/example/assignment_2_eg/view/MovieDetails.java
+++ app/src/main/java/com/example/assignment_2_eg/view/MovieDetails.java
@@ -4,16 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
-import com.example.assignment_2_eg.R;
 import com.example.assignment_2_eg.databinding.ActivityMainBinding;
 import com.example.assignment_2_eg.databinding.ActivityMovieDetailsBinding;
 import com.example.assignment_2_eg.model.MovieModel;
@@ -32,26 +27,27 @@ import okhttp3.Response;
 
 public class MovieDetails extends AppCompatActivity {
 
-    MovieModel movie;
-
-    ActivityMovieDetailsBinding binding;
-    MovieDetailsViewModel movieDetailsViewModel;
+    ActivityMovieDetailsBinding binding; // View binding to access UI elements easily
+    MovieDetailsViewModel movieDetailsViewModel; // ViewModel to manage movie details data
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        // Initializing view binding
         binding = ActivityMovieDetailsBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
 //        setContentView(R.layout.activity_movie_details);
-
+        
+        // Initializing the ViewModel to get movie details
         movieDetailsViewModel = new ViewModelProvider(this).get(MovieDetailsViewModel.class);
 
+        // Observing LiveData from ViewModel to update UI when movie details change
         movieDetailsViewModel.getMovieDetailsData().observe(this,movieData ->{
-
+            
+            // Setting movie details to the their TextViews
             binding.movietitleTV.setText(movieData.getTitle());
             binding.movieratingsTV.setText(movieData.getRating());
             binding.genreTV.setText(movieData.getGenre());
@@ -60,23 +56,24 @@ public class MovieDetails extends AppCompatActivity {
             binding.movieyearTV.setText(movieData.getYear());
             binding.movierateTV.setText(movieData.getRated());
             binding.movielanguageTV.setText(movieData.getLanguage());
+            // Loading movie poster image using Glide
             Glide.with(this).load(movieData.getPoster()).into(binding.movieposter);
 
         });
 
-        Intent intentObj = getIntent();
-
+        // Getting movie ID from the previous screen (passed through Intent)
+        Intent intentObj = getIntent(); 
         String movie_id = intentObj.getStringExtra("movie_id");
 
+        // If a movie ID was received, it'll fetch its details from the ViewModel
         if (movie_id != null) {
             movieDetailsViewModel.getMovieDetails(movie_id);
         }
 
-
-
+        // Setting click listener for the back button to close the activity
         binding.backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { // Closes the current activity and returns to the previous screen
                 finish();
             }
         });

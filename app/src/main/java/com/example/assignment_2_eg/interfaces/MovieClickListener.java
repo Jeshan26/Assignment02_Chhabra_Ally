16d6fd811a83842794f04f70fb0f0d5d44d0c8af
Aaliyah Ally <79179237+aaliyah033@users.noreply.github.com>
2025-03-28 00:06:10 -0400

    Update MovieClickListener.java

--- app/src/main/java/com/example/assignment_2_eg/interfaces/MovieClickListener.java
+++ app/src/main/java/com/example/assignment_2_eg/interfaces/MovieClickListener.java
@@ -3,6 +3,8 @@ package com.example.assignment_2_eg.interfaces;
 import android.view.View;
 
 public interface MovieClickListener {
+     //called when movie item is clicked
+    // 'view' represents the view that was clicked, and 'pos' is the position of the clicked item in the list
 
     void onClick(View view, int pos);
 }
